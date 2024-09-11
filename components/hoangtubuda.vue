c45e8e5e855aca29ab54ae9d5147a1df2b5cd7ee
hahahehehihi321 <lehoangphuc12032005@gmail.com>
2024-09-11 21:06:28 +0700

    toi oc cac

--- components/hoangtubuda.vue
+++ components/hoangtubuda.vue
@@ -1,9 +1,11 @@
 <template>
  <v-container>
-   <div>
-        <h4 class="font-weight-bold mb-6 mt-4">Choose time</h4>
-        <h4 class="subtitle-1">Lake Memphrémagog - Dock Pointe Merry</h4>
-   </div>
+   <v-row class="align-center">
+      <v-avatar color="blue" size="30">
+        <span class="white--text">1</span>
+      </v-avatar>
+      <span class="ml-3, font-weigth-bold">Choose time</span>
+    </v-row>
     <v-row align="center" justify="center">
         <v-col cols="auto">
             <v-btn @click="prevDay">
@@ -20,14 +22,14 @@
             >
                 <template v-slot:activator="{on, attrs}">
                     <v-text-field
-                        v-model="selectedDate"
+                        v-model="calendarDate"
                         readonly
                         v-bind="attrs"
                         v-on="on"
                         append-icon="mdi-calendar"
                     ></v-text-field>
                 </template>
-                <v-date-picker v-model="selectedDate" @input="menu = false"></v-date-picker>
+                <v-date-picker v-model="calendarDate" @input="menu = false"></v-date-picker>
             </v-menu>
         </v-col>
         <v-col cols="-5">
@@ -35,6 +37,9 @@
           <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
       </v-col>
+    </v-row>
+    <v-row>
+      <span class="subtitle-1">Lake Memphrémagog - Dock Pointe Merry</span>
     </v-row>
     <v-row>
         <v-col>
@@ -47,16 +52,17 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="boat in boats" :key="boat.name">
+                    <tr  v-for="boat in boats" :key="boat.name"  >
                         <td>
                             <a :href="boat.link">{{ boat.name }}</a>
                         </td>
                         <td>{{ boat.hp }}</td>
-                        <td v-for="availability in boat.availabilities" :key="availability.date">
+                      
+                        <td v-for="date in weekDates" :key="date">
                             <v-btn
+                                @click="selectDate(boat, date)"
                                 icon
-                                :color="availability.isAvailable ? 'green' : 'blue'"
-                                :disabled="!availability.isAvailable"
+                                :color="selectedDate === date && selectedBoat === boat ? 'blue' : 'green'"
                             >
                                 <v-icon>mdi-clock-outline</v-icon>
                             </v-btn>
@@ -66,7 +72,8 @@
             </v-simple-table>
         </v-col>
     </v-row>
-    <v-row class="mt-6 pl-3">
+
+    <v-row class="mt-6 pl-3 ">
       <v-btn color="primary" large @click="nextStep">Next Step
          <v-icon>mdi-chevron-right</v-icon>
       </v-btn>
@@ -79,67 +86,57 @@
   data() {
     return {
       menu: false,
-      selectedDate: new Date().toISOString().substr(0, 10),
       boats: [
         {
           name: "Vectra 21",
           link: "#",
           hp: 60,
-          availabilities: [
-            { date: "18 August", isAvailable: true },
-            { date: "19 August", isAvailable: true },
-            { date: "20 August", isAvailable: true },
-            { date: "21 August", isAvailable: true },
-            { date: "22 August", isAvailable: true },
-            { date: "23 August", isAvailable: true },
-            { date: "24 August", isAvailable: true },
-          ],
         },
         {
           name: "Sportfisher 21",
           link: "#",
           hp: 60,
-          availabilities: [
-            { date: "18 August", isAvailable: true },
-            { date: "19 August", isAvailable: true },
-            { date: "20 August", isAvailable: false },
-            { date: "21 August", isAvailable: true },
-            { date: "22 August", isAvailable: true },
-            { date: "23 August", isAvailable: true },
-            { date: "24 August", isAvailable: true },
-          ],
         },
       ],
+      selectedDate: null,
+      selectedBoat: null,
+      dayOffset: 0
     };
   },
   computed: {
     weekDates() {
       const dates = [];
-      for (let i = 0; i < 7; i++) {
-        const date = new Date();
-        date.setDate(date.getDate() + i);
-        dates.push(`${date.getDate()} ${date.toLocaleString('default', { month: 'long' })}`);
+      for (let i = 0; i < 13; i++) {
+        const date = new Date()
+        date.setDate(date.getDate() + this.dayOffset + i);
+        dates.push(`${date.getDate()} ${date.toLocaleString('en-CA', { month: 'long' })}`);
       }
       return dates;
     },
+    calendarDate() {
+      const date = new Date()
+      date.setDate(date.getDate() + this.dayOffset);
+      const dates = `${date.getDate()} ${date.toLocaleString('en-CA', { month: 'long' })}`;
+      return dates
+    }
   },
   methods: {
     prevDay() {
       // Function to handle previous day selection
-      const date = new Date(this.selectedDate);
-      date.setDate(date.getDate() - 1);
-      this.selectedDate = date.toISOString().substr(0, 10);
+      this.dayOffset -= 7
     },
     nextDay() {
       // Function to handle next day selection
-      const date = new Date(this.selectedDate);
-      date.setDate(date.getDate() + 1);
-      this.selectedDate = date.toISOString().substr(0, 10);
+      this.dayOffset += 7
     },
     nextStep() {
       // Function to handle the next step action
       alert("Proceeding to the next step...");
     },
+    selectDate (boat, date) {
+      this.selectedBoat = boat
+      this.selectedDate = date
+    }
   },
 };
 </script>
