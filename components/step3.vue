3d1295bc56e84de775cc9334e099e2476108f005
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2024-09-23 19:19:00 +0700

    YESSSSSSSSSSSSS

---
+++ components/step3.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-card
+            class="mb-12"
+            color="grey lighten-1"
+            height="200px"
+             ></v-card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
-    <div height="100%">
+    <div class="ml-4" height="100%">
       <v-stepper v-model="e1" height="100%" width="100%">
-        <v-stepper-header class="">
+        <v-stepper-header class="" outlined>
           <v-divider class="divider1" ></v-divider>
             <v-stepper-step 
             :complete="e1 > 1"
-            step="1" class="pa-0 ">
+            step="1" class="num pa-0 font-weight-bold"  >
           </v-stepper-step>
           <v-divider class="divider" :class="e1 >=2 ? 'active' : 'inactive' "></v-divider>
             <v-stepper-step
             :complete="e1 > 2"
-            step="2" class="pa-0">
+            step="2" class="num font-weight-bold pa-0 ">
             </v-stepper-step>
-          <v-divider class="divider"></v-divider>
+            <v-divider class="divider" :class="e1 >=3 ? 'active' : 'inactive' "></v-divider>
         <v-stepper-step :complete="e1 > 3"
-          step="3" class="pa-0">
+          step="3" class="num font-weight-bold pa-0">
         </v-stepper-step>
-          <v-divider class="divider"></v-divider>
+        <v-divider class="divider" :class="e1 >=4 ? 'active' : 'inactive' "></v-divider>
             <v-stepper-step 
-              step="4" class="pa-0" style="margin-right:87px">
-        </v-stepper-step>
+              step="4" class="num font-weight-bold pa-0" style="margin-right:87px">
+            </v-stepper-step>
   </v-stepper-header>
     <v-stepper-items>
       <v-stepper-content class="" height="1000" step="1">
@@ -39,6 +39,30 @@
       <v-stepper-content step="2">
             <Step2>
             </Step2>
+            <div class="3btn" style="margin-top: 100px ;margin-left: 18px;">
+        <v-btn
+          color="primary"
+          @click="e1 = 3"
+          class="font-weight-bold mt-5">
+          NEXT STEP >
+        </v-btn>       
+      </div>
+          </v-stepper-content>
+          <v-stepper-content step="3">
+            <Step3>
+            </Step3>
+            <div class="4btn" style="margin-top: 100px ;margin-left: 18px;">
+        <v-btn
+          color="primary"
+          @click="e1 = 4"
+          class="font-weight-bold mt-5">
+          NEXT STEP >
+        </v-btn>       
+      </div>
+          </v-stepper-content>
+          <v-stepper-content step="4">
+            <Step4>
+            </Step4>
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
@@ -49,6 +73,9 @@
 <script>
 
 import Step1 from '../components/step1.vue';
+import Step2 from '../components/step2.vue';
+import Step3 from '../components/step3.vue';
+import Step4 from '../components/step4.vue';
 
 export default {
     data () {
@@ -65,6 +92,10 @@ export default {
   height: 10px;
   border: none;
 }
+::v-deep .v-stepper__step__step {
+min-width: 33px;
+min-height: 33px;
+}
 .divider1 {
   background-color: black;
   max-height: 100px;
@@ -72,12 +103,27 @@ export default {
   border: none;
   
 }
+::v-deep .theme--light.v-stepper .v-stepper__step:not(.v-stepper__step--active):not(.v-stepper__step--complete):not(.v-stepper__step--error) .v-stepper__step__step {
+  background: aqua ;
+  color: black;
+}
+::v-deep .theme--light.v-stepper .v-stepper__step__step {
+    color: white;
+}
+
+::v-deep .v-stepper__step--active .v-stepper__step__step{
+  background-color: black !important;
+}
+::v-deep .theme--light.v-stepper .v-stepper__step:not(.v-stepper__step--active):not(.v-stepper__step--error):not(.v-stepper__step--inactive) .v-stepper__step__step {
+  background-color: black !important;
+}
+
 .inactive {
-  background-color: aqua;
+  background-color: aqua !important;
 }
 
 .active {
-  background-color: black;
+  background-color: black !important;
 }
 
 </style>
