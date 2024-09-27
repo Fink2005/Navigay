5fc1e36faf41555e4d2af0cf119ddd5ddc966e65
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2024-09-27 16:41:33 +0700

    step2

--- components/step2.vue
+++ components/step2.vue
@@ -1,7 +1,12 @@
 <template>
     <v-card
             class="mb-12"
-            color="grey lighten-1"
+            color="white lighten-1"
             height="200px"
-             ></v-card>
+             >
+             <div class="d-flex">
+                <div class="d-flex align-center justify-center v-sheet rounded-pill deep-orange lighten-2 font-weight-bold" style="height: 40px; width: 40px;">2</div>
+                <div class="d-flex ml-3 mt-2 font-weight-bold">Selected trip</div>
+             </div>
+            </v-card> 
 </template>
